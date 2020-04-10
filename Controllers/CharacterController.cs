3fa652869c92bf57f466672215cb57eba8b2cc07
Patrick <patrick@godknows.de>
2020-04-10 09:17:30 +0200

    Routing with Parameters

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,7 +11,7 @@ namespace dotnet_rpg.Controllers
     {
         private static List<Character> characters = new List<Character> {
             new Character(),
-            new Character { Name = "Sam"}
+            new Character { Id = 1, Name = "Sam"}
         };
         
         [HttpGet("GetAll")]
@@ -19,9 +20,10 @@ namespace dotnet_rpg.Controllers
             return Ok(characters);
         }
 
-        public IActionResult GetSingle()
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
         {
-            return Ok(characters[0]);
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
     }
 }

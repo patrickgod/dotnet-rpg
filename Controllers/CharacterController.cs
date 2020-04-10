ec20e6648a28d8556eeefbe0417d8ccea7d31ef2
Patrick <patrick@godknows.de>
2020-04-10 09:24:57 +0200

    Add a New Character with POST

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -7,13 +7,13 @@ namespace dotnet_rpg.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CharacterController: ControllerBase
+    public class CharacterController : ControllerBase
     {
         private static List<Character> characters = new List<Character> {
             new Character(),
             new Character { Id = 1, Name = "Sam"}
         };
-        
+
         [HttpGet("GetAll")]
         public IActionResult Get()
         {
@@ -24,6 +24,13 @@ namespace dotnet_rpg.Controllers
         public IActionResult GetSingle(int id)
         {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
         }
     }
 }

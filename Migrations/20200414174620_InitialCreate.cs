8cd65bde7ab644683fcefcf77705aaaf37857b2d
Patrick <patrick@godknows.de>
2020-04-14 19:51:32 +0200

    First Migration

---
+++ Migrations/20200414174620_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_rpg.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    HitPoints = table.Column<int>(nullable: false),
+                    Strength = table.Column<int>(nullable: false),
+                    Defense = table.Column<int>(nullable: false),
+                    Intelligence = table.Column<int>(nullable: false),
+                    Class = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
